e85ab6f95a9da621d9990a48ef97228177dc8aa4
Alexander Krasilnikov <i@krasilnikov-1.ru>
2023-02-18 17:38:47 +0300

    Some change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int account = 300;
+        int deposit = 1700;
+
+        int bonus;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int TotalAmount = account + deposit + bonus;
+
+            System.out.println("Общая сумма:" + TotalAmount + "р.");
+            System.out.println("Бонусных:" + bonus + "р.");
+
     }
 }
